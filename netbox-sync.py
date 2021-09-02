a220f77b886fc1665a91a77a1a104d6ce8e9b932
ricardo.bartels@telekom.de <ricardo.bartels@telekom.de>
2021-09-02 23:56:00 +0200

    prepares release Grace.1 1.1.1

--- netbox-sync.py
+++ netbox-sync.py
@@ -25,8 +25,8 @@ from module.netbox.object_classes import *
 from module.sources import instantiate_sources
 
 
-__version__ = "1.1.0"
-__version_date__ = "2021-06-30"
+__version__ = "1.1.1"
+__version_date__ = "2021-09-02"
 __author__ = "Ricardo Bartels <ricardo.bartels@telekom.de>"
 __description__ = "NetBox Sync"
 __license__ = "MIT"
