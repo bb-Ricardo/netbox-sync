e230215412381c2478beb56b52bde9dd5718af26
ricardo.bartels@telekom.de <ricardo.bartels@telekom.de>
2022-08-25 00:24:15 +0200

    bumps 1.3.0-beta1 version and improves docker publish

--- .publi.sh
+++ .publi.sh
@@ -1,16 +1,40 @@
 #!/usr/bin/env bash
 
-[[ -z "$1" ]] && echo "please define a version tag" && exit
-
 IMAGE_NAME="bbricardo/netbox-sync"
+IMAGE_PLATFORM="linux/arm/v7,linux/arm64/v8,linux/amd64"
+IMAGE_TAG=$(grep "^__version__" netbox-sync.py | sed 's/__version__ = "\(.*\)"/\1/g')
 
+if [[ -z "$IMAGE_TAG" ]]; then
+  echo "ERROR: unable to grep version from 'netbox-sync.py'"
+  exit 1
+fi
+
+read -p "Is '$IMAGE_TAG' a beta (b) release or final (f) release: " -n1 ANSWER && echo
+
+[[ $ANSWER =~ [bB] ]] && FINAL=false
+[[ $ANSWER =~ [fF] ]] && FINAL=true
+[[ -z "${FINAL+default}" ]] && echo "Please select 'b' or 'f'." && exit 1
+
+unset DOCKER_TLS_VERIFY
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+
+find . -name "__pycache__" -delete
 docker --config ./docker-tmp login
 docker --config ./docker-tmp buildx create --use
-docker --config ./docker-tmp buildx build --push \
-  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  --tag ${IMAGE_NAME}:latest \
-  --tag ${IMAGE_NAME}:${1} .
+if [[ $FINAL ]]; then
+  docker --config ./docker-tmp buildx build --push \
+    --platform ${IMAGE_PLATFORM} \
+    --tag ${IMAGE_NAME}:latest \
+    --tag ${IMAGE_NAME}:${IMAGE_TAG} .
+  [[ $? -ne 0 ]] && exit 1
+  which docker-pushrm >/dev/null 2>&1 &&  docker-pushrm ${IMAGE_NAME}:latest
+else
+  docker --config ./docker-tmp buildx build --push \
+    --platform ${IMAGE_PLATFORM} \
+    --tag ${IMAGE_NAME}:${IMAGE_TAG} .
+fi
 
-rm rf ./docker-tmp
+rm -rf ./docker-tmp
 
 # EOF
